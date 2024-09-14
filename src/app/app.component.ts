e8dc1b67078a04fe181cd1c3e203a0220d24dc4f
Andy Huynh <andyhuynh942@gmail.com>
2024-09-13 22:37:18 -0400

    experiment with material angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { TestTableComponent } from './test-table/test-table.component';
+import { TestDashboardComponent } from './test-dashboard/test-dashboard.component';
+import { TestTreeComponent } from './test-tree/test-tree.component';
+import { TestDragDropComponent } from './test-drag-drop/test-drag-drop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolBarComponent, TestTreeComponent, TestTableComponent, TestDragDropComponent,TestDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/tool-bar/tool-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToolBarComponent } from './tool-bar.component';
+
+describe('ToolBarComponent', () => {
+  let component: ToolBarComponent;
+  let fixture: ComponentFixture<ToolBarComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
